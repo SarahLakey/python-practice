f35084579708a8c0bb4b72096ca5b53edf5d2fa3
SarahLakey <salbers1212@gmail.com>
2024-09-15 21:46:14 -0500

    practiced using variables - strings and integers

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 #Let's practice learning Python
 
-print("Ice cream is the best!")
+# print("Ice cream is the best!")
+
+#String 
+first_name = "Sarah"
+food="mac and cheese"
+email = "Sarah@fake.com"
+
+print(first_name)
+print(f"Do you like {food}?")
+print(f"Your email is {email}.")
+
+# Integers (whole numbers - would be a float if not)
+age = 34
+quantity = 3
+num_of_students = 8
+
+print(f"You are {age} years old.")
+print(f"You are buying {quantity} cupcakes.")
+print(f"My old class had {num_of_students} students.")
